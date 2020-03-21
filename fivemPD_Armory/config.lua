85c812d3876857596d6c5bdaca671a638e26cadf
constantgillet <constant.gillet@hetic.net>
2020-03-21 17:42:28 +0100

    :sparkles:

---
+++ fivemPD_Armory/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.PointMarker = {
+	r = 0, g = 0, b = 255,
+	x = 1.5, y = 1.5, z = 1.0
+}
+
+Config.PoliceArmories = {
+	{ x = 452.24, y = -980.18, z = 29.69}
+}
+
+Config.Weapons = {
+    {Label = "Flashlight", Hash = "weapon_flashlight", Ammo=100},
+    {Label = "Nightstick", Hash = "weapon_nightstick", Ammo=100},
+    {Label = "Pistol", Hash = "weapon_pistol", Ammo=100},
+    {Label = "Pistol Mk II", Hash = "weapon_pistol_mk2", Ammo=100},
+    {Label = "Combat Pistol", Hash = "weapon_combatpistol", Ammo=100},
+    {Label = "Stun Gun", Hash = "weapon_stungun", Ammo=100},
+    {Label = "SMG", Hash = "weapon_smg", Ammo=100},
+    {Label = "Pump Shotgun", Hash = "weapon_pumpshotgun", Ammo=100},
+    {Label = "Carbine Rifle", Hash = "weapon_carbinerifle", Ammo=100},
+    {Label = "Special Carbine", Hash = "weapon_specialcarbine", Ammo=100},
+    {Label = "BZ Gas", Hash = "weapon_bzgas", Ammo=100}
+}
